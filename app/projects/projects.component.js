fd4c831499f8c7b2a6adbf1fabbcb3138b8d494b
Andy Herzog <asherzog19@gmail.com>
2017-02-09 12:47:10 -0700

    :art: created projects component

---
+++ app/projects/projects.component.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .component('projects', {
+      controller: 'projectsController',
+      templateUrl: '../../app/projects/projects.template.html'
+    });
+
+}());
