cab3786255766bf6334fe464568a1fce98bee993
Andy Herzog <asherzog19@gmail.com>
2017-02-19 14:40:25 -0700

    :neckbeard: gradient background color

--- app/projects/projects.controller.js
+++ app/projects/projects.controller.js
@@ -7,7 +7,12 @@
 
   function projectsController() {
     const vm = this;
-    document.body.style.background = 'black';
+    document.body.style = `background: linear-gradient(35deg, #292c2b, #041340, #262d2b);
+                            background-size: 600% 600%;
+
+                            -webkit-animation: projects 17s ease infinite;
+                            -moz-animation: projects 17s ease infinite;
+                            animation: projects 17s ease infinite;`;
   }
 
 }());
