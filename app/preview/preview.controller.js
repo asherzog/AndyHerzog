e52d729c0f762a2949a1a8cf58462c0e499517de
Andy Herzog <asherzog19@gmail.com>
2017-02-19 15:10:59 -0700

    :ok_hand: images to gifs

--- app/preview/preview.controller.js
+++ app/preview/preview.controller.js
@@ -13,26 +13,23 @@
         title: 'Spot Ninja',
         url: 'https://spot-ninja.firebaseapp.com/',
         github: 'https://github.com/teamBetterThanAwesome/Spot-NinjaFrontEnd',
+        image: '../../assets/spotninja.png',
         gif: '../../assets/spotninja.gif'
       },
       {
         title: 'Poetry Ipsum',
         url: 'https://poetry-ipsum.firebaseapp.com/',
         github: 'https://github.com/asherzog/Poetry-Ipsum',
+        image: '../../assets/poetry.png',
         gif: '../../assets/poetry.gif'
       },
       {
         title: 'DonorsComplete',
         url: 'https://donorscomplete.firebaseapp.com/',
         github: 'https://github.com/asherzog/DonorsComplete',
+        image: '../../assets/donors.png',
         gif: '../../assets/donors.gif'
-      },
-      // {
-      //   title: 'DonorsComplete',
-      //   url: 'https://donorscomplete.firebaseapp.com/',
-      //   github: 'https://github.com/asherzog/DonorsComplete',
-      //   gif: '../../assets/donors.gif'
-      // }
+      }
     ];
 
   }
