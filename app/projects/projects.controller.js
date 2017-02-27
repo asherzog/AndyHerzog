da61be1f7dcd24fb7f58dfdd3b5cb09049b80eb3
Andy Herzog <asherzog19@gmail.com>
2017-02-27 12:17:42 -0700

    safari style changing

--- app/projects/projects.controller.js
+++ app/projects/projects.controller.js
@@ -7,7 +7,7 @@
 
   function projectsController() {
     const vm = this;
-    document.body.style.backgroundColor = null;
+    document.body.style = null;
     document.body.classList.add('project-class')
     // document.body.style = `background: linear-gradient(118deg, #060a1d, #1e6369, #0a0d2d);
     //                         background-size: 600% 600%;
