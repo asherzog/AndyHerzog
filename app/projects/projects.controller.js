7c9a80fd4ede89956d925860be6fcb2b2e67e08e
Andy Herzog <asherzog19@gmail.com>
2017-02-27 10:30:39 -0700

    safari fix hopefully

--- app/projects/projects.controller.js
+++ app/projects/projects.controller.js
@@ -7,12 +7,12 @@
 
   function projectsController() {
     const vm = this;
-    document.body.style = `background: linear-gradient(118deg, #060a1d, #1e6369, #0a0d2d);
-                            background-size: 600% 600%;
+    document.body.style.background = `linear-gradient(118deg, #060a1d, #1e6369, #0a0d2d);`
+    document.body.style.backgroundSize =  `600% 600%;`
+    document.body.style.animation = `animation: project 24s ease infinite;`
+                            // -webkit-animation: project 24s ease infinite;
+                            // -moz-animation: project 24s ease infinite;
 
-                            -webkit-animation: project 24s ease infinite;
-                            -moz-animation: project 24s ease infinite;
-                            animation: project 24s ease infinite;`;
   }
 
 }());
