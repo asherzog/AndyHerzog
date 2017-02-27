fde4df714f29d352f20235b8c5130bb1ccc5cc5d
Andy Herzog <asherzog19@gmail.com>
2017-02-27 12:35:43 -0700

    got 99 problems and safari is all of them

--- app/projects/projects.controller.js
+++ app/projects/projects.controller.js
@@ -7,14 +7,14 @@
 
   function projectsController() {
     const vm = this;
-    // document.body.style = null;
-    // document.body.classList.add('project-class')
-    document.body.style = `background: linear-gradient(118deg, #060a1d, #1e6369, #0a0d2d);
-                            background-size: 600% 600%;
-
-                            -webkit-animation: project 24s ease infinite;
-                            -moz-animation: project 24s ease infinite;
-                            animation: project 24s ease infinite;`;
+    document.body.style = null;
+    document.body.classList.add('project-class')
+    // document.body.style = `background: linear-gradient(118deg, #060a1d, #1e6369, #0a0d2d);
+    //                         background-size: 600% 600%;
+    //
+    //                         -webkit-animation: project 24s ease infinite;
+    //                         -moz-animation: project 24s ease infinite;
+    //                         animation: project 24s ease infinite;`;
   }
 
 }());
