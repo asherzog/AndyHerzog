4f88e0dfc1540a4123d070b3ac97ff01d7832973
Andy Herzog <asherzog19@gmail.com>
2017-03-02 10:25:06 -0700

    :rocket: Hash routing instead of clean

--- app.config.js
+++ app.config.js
@@ -6,7 +6,6 @@
   config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
 
   function config($stateProvider, $urlRouterProvider, $locationProvider){
-    $locationProvider.html5Mode(true)
 
     $stateProvider
       .state({
